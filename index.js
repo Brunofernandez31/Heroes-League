069c3f08f2c5f4652086698aeab44ada1bc7a33c
Bruno Fernandez <bruno.fernandez@oclock.school>
2025-11-23 15:15:13 +0100

    Heroes league - back end - components et leurs routes

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ app.get('/' , (req,res) => {
     res.render("index")
 })
 
+app.get('/services' , (req,res) => {
+    res.render("services")
+})
+
+app.get('/nos-heros' , (req,res) => {
+    res.render("nos-heros")
+})
+
+app.get('/temoignages' , (req,res) => {
+    res.render("temoignages")
+})
+
+app.get('/sauvez-moi' , (req,res) => {
+    res.render("sauvez-moi")
+})
+
 app.listen(3000, () => {
     console.log("Le serveur tourne sur le port 3000")
 })
