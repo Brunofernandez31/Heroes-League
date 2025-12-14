55c9c83c180f51a95117196140f11c97ff1c8f48
Bruno Fernandez <bruno.fernandez@oclock.school>
2025-12-14 16:11:39 +0100

    Creation continuation du fichier sql

--- Data/heroes.js
+++ Data/heroes.js
@@ -5,7 +5,7 @@ export default [
         img: "./ressources/images/catastrophic.png",
         description: `Avec son super ronron, il saura vous réconforter dans n'importe quelle situation.
 
-Ne contrôle pas tout à fait sa patte droite, et peut être amené à vous mettre une - plus ou moins petite - tape sur la tête.`
+Ne contrôle pas tout à fait sa patte droite, et peut être amené à vous mettre une - plus ou moins petite - tape sur la tête. Se perd de temps en temps`
 
         },
     {
@@ -47,5 +47,31 @@ Peut être amené à vous demander de financer son prochain album, ne pas accept
         description: `Un vieu super-héro qui ne veut pas partir à la retraite.
 
 N'a plus de pouvoir mais refuse de l'admettre, peut être amené à vous raconter des histoires de l'époque où il était encore super.`,
+    },{
+        article: "capitaine_glue",
+        name: "Capitaine Glue",
+        img: "./ressources/images/capitaine_glue.png",
+        description: `Peut coller n'importe quoi à n'importe quoi. Très utile pour réparer, immobiliser des ennemis, ou recoller les pots cassés… au sens propre.
+
+Tout ce qu'il touche reste collé pendant 48h. Y compris vous, si vous lui serrez la main.`,
+    },{
+        article: "prune_power",
+        name: "Prune Power",
+        img: "./ressources/images/prune_power.png",
+        description: `Spécialiste des situations coincées. Peut vous sortir d'une contravention injuste ou d'une constipation rebelle grâce à ses pouvoirs laxatifs naturels.
+
+Ne fait pas la différence entre les problèmes métaphoriques et digestifs. Peut résoudre votre dispute administrative en vous donnant la diarrhée.`,
+    },{
+        article: "fidelidog",
+        name: "Fidélidog",
+        img: "./ressources/images/fidelidog.png",
+        description: `Sens du devoir surdéveloppé et flair infaillible pour les gens en détresse. Peut vous retrouver n'importe où, vous protéger de n'importe quoi, et rapporter n'importe quel objet perdu.
+
+Toujours fidèle, parfois trop. A tendance à "sauver" les gens qui n'ont rien demandé, surtout les facteurs, les livreurs, et les chats qu'il considère en danger.`,
+    },{
+    },{
+
+    },{
+
     }
 ]
--- Data/heroes_db.sql
+++ Data/heroes_db.sql
@@ -13,6 +13,8 @@ CREATE table "user" (
 CREATE table "stuff" (
     id_stuff SERIAL PRIMARY KEY,
     description TEXT,
+    article VARCHAR(255),
+    description_article TEXT,
     statut BOOLEAN NOT NULL,
     stock SMALLINT NOT NULL
 );
@@ -24,7 +26,7 @@ CREATE table "hero" (
     disadvantage TEXT,
     price_per_mission DECIMAL (7,2) NOT NULL, 
     percent_win DECIMAL (5,2) CHECK (percent_win BETWEEN 0 AND 100), 
-    id_admin INT NOT NULL REFERENCES admin(id_admin)
+    id_admin INT NOT NULL REFERENCES "admin"(id_admin)
 );
 
 CREATE table "mission" (
@@ -35,26 +37,87 @@ CREATE table "mission" (
     city VARCHAR (255) NOT NULL,
     start_date DATE,
     duration INTERVAL NOT NULL,
-    id_user REFERENCES user(id_user),
-    id_hero REFERENCES hero(id_hero)
+    id_user INT NOT NULL REFERENCES "user"(id_user),
+    id_hero INT NOT NULL REFERENCES "hero"(id_hero)
 );
 
 CREATE table "opinion" (
     id_opinion SERIAL PRIMARY KEY,
     description TEXT,
     score SMALLINT,
-    id_user INT NOT NULL REFERENCES user(id_user),
-    id_hero INT NOT NULL REFERENCES hero(id_hero)
+    id_user INT NOT NULL REFERENCES "user"(id_user),
+    id_hero INT NOT NULL REFERENCES "hero"(id_hero)
 );
 
 CREATE table "describe" (
-    id_hero INT NOT NULL REFERENCES hero(id_hero),
-    id_stuff INT NOT NULL REFERENCES stuff(id_stuff),
+    id_hero INT NOT NULL REFERENCES "hero"(id_hero),
+    id_stuff INT NOT NULL REFERENCES "stuff"(id_stuff),
     PRIMARY KEY (id_hero, id_stuff)
 );
 
 CREATE table "have" (
-    id_hero INT NOT NULL REFERENCES hero(id_hero),
-    id_stuff INT NOT NULL REFERENCES stuff(id_stuff),
+    id_hero INT NOT NULL REFERENCES "hero"(id_hero),
+    id_stuff INT NOT NULL REFERENCES "stuff"(id_stuff),
     PRIMARY KEY (id_hero, id_stuff)
 );
+
+INSERT INTO "amdin" (name, email)
+VALUES (BruBru, brubru@heroes_leagues.fr);
+
+INSERT INTO "user" (name, email)
+VALUES (client, client@heroes_leagues.fr);
+
+INSERT INTO "stuff" (article, description_article, statut, stock, manufacturing_time)
+VALUES 
+    -- Cat Astrophic
+    ('Balles de laine anti-stress', 'Pour le confort des victimes', TRUE, 3, '3 days'),
+    ('Griffoir géant portatif', 'Pour les griffades d''urgence', TRUE, 2, '5 days'),
+    ('Collier GPS', 'Pour retrouver Cat Astrophic', TRUE, 1, '10 days'),
+    
+    -- Carotte Woman
+    ('Calculatrice truquée', 'Pour maitriser toutes les transactions frauduleuses', TRUE, 1, '1 day'),
+    ('Faux contrats', 'En double exemplaire', TRUE, 2, '6 hours'),
+    ('Éplucheur de secours', 'Au cas où on a du mal à s''épeler', TRUE, 1, '12 hours'),
+    
+    -- Ultraquenarde
+    ('Carte des pièges', 'Périmée de 10 ans', TRUE, 1, '5 days'),
+    ('Corde d''escalade emmêlée', 'Très pratique pour perdre du temps', TRUE, 2, '1 day'),
+    ('Boussole déréglée', 'Pointe toujours vers le sud', TRUE, 3, '2 days'),
+    
+    -- Poulpy
+    ('Ventouses de rechange', 'Quand les autres tombent ou pour accrocher son téléphone', TRUE, 50, '3 hours'),
+    ('Déodorant spécial tentacules', 'Efficacité très limitée', TRUE, 5, '1 day'),
+    ('Cartons de déménagement', 'Tachés mais pré scotché', TRUE, 8, '30 minutes'),
+    
+    -- Orang Wu-Tang Clan
+    ('Micro cassé', 'A trop servi en tant que projectile', TRUE, 2, '2 days'),
+    ('Démo CD', 'Mon prochain album, sert plus de miroir qu''autre chose', TRUE, 20, '1 hour'),
+    ('Banane géante', 'Pour les cascades ou faire déraper les véhicules', TRUE, 4, '6 hours'),
+    
+    -- Superimé 
+    ('Canne à pommeau magique', 'Qui ne marche plus mais sert de béquille', TRUE, 1, '4 days'),
+    ('Album photo années 40', 'Pour vous raconter sa vie', TRUE, 1, '1 day'),
+    ('Prothèse auditive', 'Pour le style, n''est jamais allumée', TRUE, 2, '2 days'),
+    
+    -- Capitaine Glue
+    ('Tube de décollant industriel', 'Souvent vide et pas forcément fonctionnel', TRUE, 4, '1 day'),
+    ('Gants antiadhésifs', 'Pour que les clients ne restent pas scotchés à leurs affaires', TRUE, 2, '12 hours'),
+    ('Spatule éclatante', 'Pour décoller ce qui colle', TRUE, 1, '18 hours'),
+    
+    -- Prune Power
+    ('Code de la route', 'Annoté avec des taches et des fautes', TRUE, 1, '3 days'),
+    ('Papier toilette de secours', 'Très important et parfum mirabelle', TRUE, 20, '1 hour'),
+    ('Stylo gommeur', 'Pour contester les PV', TRUE, 3, '2 hours'),
+    
+    -- Fidelidog
+    ('Laisse XXL', 'Qu''il traîne derrière lui', TRUE, 1, '1 day'),
+    ('Os en caoutchouc indestructible', 'Son préféré', TRUE, 5, '6 hours'),
+    ('Collier avec médaille', 'NE PAS NOURRIR - EN SERVICE', TRUE, 1, '2 days'),
+    
+    -- Objets génériques
+    ('Kit de premier secours', 'Entamé', TRUE, 10, '1 day'),
+    ('Talky Walky', 'Voyant de batterie HS, on ne sait pas à l''avance s''ils fonctionnent', TRUE, 6, '12 hours'),
+    ('Cape altérée', 'Déchirée et pas toujours lavée mais elle tient chaud', TRUE, 3, '1 day'),
+    ('The Mask', 'Vous serez d''humeur cartoon avec une voix modifiée', TRUE, 1, '7 days'),
+    ('Lano', 'Pratique pour se marier en urgence mais ils SAURON que vous l''avez', TRUE, 1, '14 days'),
+    ('Cape Huccino', 'Elle fait le café mais augmente votre tension, pas pour les cardiaques', TRUE, 3, '8 hours');
