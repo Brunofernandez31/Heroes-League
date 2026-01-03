5f906e7eea78e276e71a9596a3f508bdb14db516
Bruno Fernandez <bruno.fernandez@oclock.school>
2026-01-03 20:17:01 +0100

    calcul du prix total sur le rapport de mission du hero

--- public/script/rapport.script.js
+++ public/script/rapport.script.js
@@ -7,27 +7,53 @@ const supplementImmediate = document.getElementById("urgency_immediate");
 const total = document.getElementById("total_price");
 
 
-// 'change' permet d'écouter l'evenement quand l'utilisateur change de héro
-hero.addEventListener('change', () => {
-    // 
-    const optionSelectionnee = hero.options[hero.selectedIndex];
-    const prix = optionSelectionnee.dataset.price
-    tauxHoraire.value = prix // Afficher la value dans l'input text du taux horaire du héro
-});
-
 function totalPrice () {
     const prixHero = tauxHoraire.value;
     const duree = duration.value;
 
     let supplement = 0;
-    if() {
-
+    if(supplementNull.checked) { // Vérifier quel bouton radio est coché grâce à "checked"
+        supplement = 0;
     } else if (supplementThreeDays.checked) {
         supplement = 0.05
     } else if (supplementImmediate.checked) {
         supplement = 0.15
     }
 
-    const totalPrice = prixHero * duree * radioUrgency * supplement;
-    total = Number(totalPrice.value)
-}
+    // Calcul du prix total
+    const totalPrice = prixHero * duree * ( 1+supplement); // On met 1 pour le pourcentage
+    // Si on met pas le 1 alors Résultat : 50 × 2 × 0.05 = 5€
+    // Correction : Résultat : 50 × 2 × (1 + 0.05) = 105€
+    total.value = totalPrice // Envoyé à l'input type texte la value pour le prix total
+};
+
+
+// Il faut mettre un ecouteur d'evenement à chaque fois qu'on peut changer un element influençant le prix
+// Et appeler la fonction pour le prix total
+
+supplementNull.addEventListener('change', () => {
+    totalPrice()
+});
+
+supplementThreeDays.addEventListener('change', () => {
+    totalPrice()
+});
+
+supplementImmediate.addEventListener('change', () => {
+    totalPrice()
+});
+
+duration.addEventListener('change', () => {
+    totalPrice()
+});
+
+
+// 'change' permet d'écouter l'evenement quand l'utilisateur change de héro
+hero.addEventListener('change', () => {
+    const optionSelectionnee = hero.options[hero.selectedIndex];
+    const prix = optionSelectionnee.dataset.price
+    tauxHoraire.value = prix // Afficher la value dans l'input text du taux horaire du héro
+
+    // Appelle de la fonction pour afficher le prix total
+    totalPrice()
+});
